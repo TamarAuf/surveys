d9497087d82b1e2c2b9eb6b02db602ceaf5f4adc
raz <raztamim@gmail.com>
2022-11-10 17:56:10 +0200

    updated readmen file

---
+++ src/components/B1.js
@@ -0,0 +1,8 @@
+
+const B1 = () =>{
+    return (
+        <div>
+         <h3>B1 </h3>
+        </div>
+      );
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,20 @@
+
+
+
+
+const Intro = ({nextStep}) => {
+
+
+
+  return (
+    
+    <div>
+     <h3>  Welcome to MeNow! - </h3>
+     <h2>an AI-powered platform predicting </h2>
+     <h2>predicting highly individual skincare recommendations.</h2>
+     <button onClick={nextStep}>Take the suervy</button>
+    </div>
+  );
+}
+
+export default Intro
