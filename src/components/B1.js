0a8b79d4b7d61cf78f252438d9ce27fc45d38f44
raz <raztamim@gmail.com>
2022-11-14 14:50:01 +0200

    design for intro

--- src/components/B1.js
+++ src/components/B1.js
@@ -1,7 +1,13 @@
+//raz
 const B1 = ({nextStep}) =>{
     return (
-        <div>
-         <h1>B1 info</h1>
+        <div claaName= "B1-container">
+          <hedar>
+          <h3>Hi, I’m MeNow! AI-consultant</h3>
+          <h2>Let’s start with getting to know you.</h2>
+          </hedar>
+          <label>What should we call you?</label>
+          <input type="text"></input>
          <button onClick={nextStep}>Next</button>
         </div>
       );
