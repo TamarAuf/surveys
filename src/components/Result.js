9d24823e70d93afb31f3823b2b9df288ca167b30
Tamar <tamartda@gmail.com>
2022-11-16 14:29:11 +0200

    added content and design

---
+++ src/components/Result.js
@@ -0,0 +1,17 @@
+export const Result = () => {
+  return (
+    <div className="Result-container">
+        <header>
+            <h1 className="title">[Name], your personal recommendations are ready!</h1>
+            <p className="subtitle">Thank you for your time and for completing the questionnaire thoughfully. </p>
+
+            <p className="subtitle">After analyzing your answers I found out that you have [type] skin type <br />
+            and we need to focus on [abc] and [abc]. I compared the ingredient <br />
+            impact with your skin parameters and selected the most suitable <br />
+            individual skin regime. </p>
+        </header>
+    </div>
+  )
+}
+
+export default Result
