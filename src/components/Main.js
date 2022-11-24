a47b59b0bfd38257d2f955008d5b671ed1bee3d8
raz <raztamim@gmail.com>
2022-11-24 20:08:48 +0200

    fixed the upadte to the responses object

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ import Result from "./Result"
 const Form = () => {
   const [page, setPage] = useState(0);
 
-  const [Responses,setResponses]  = useState({
+  let [Responses,setResponses]  = useState({
       B1Responses : {
          name: "raz",
          age: "",
@@ -85,7 +85,7 @@ const Form = () => {
             return(<Intro
               nextStep={nextStep}/>);
         case 1:
-            return(<B1  responses={Responses.B1Responses}  setResponses={setResponses} questions={Questions.B1Questions.questions} options={Questions.B1Questions}
+            return(<B1  responses={Responses} setResponses={setResponses} questions={Questions.B1Questions.questions} options={Questions.B1Questions}
               nextStep={nextStep}/> );   
         case 2:
             return(<B2
