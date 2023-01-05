fa7b252a0d0d4ca4af8eeef15c2db16f0cbb658f
zemira kehati <kehati10@gmail.com>
2023-01-05 12:35:19 +0200

    added login and signup routes to main router

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,11 @@
-/* eslint-disable linebreak-style */
 const router = require('express').Router();
 
+const login = require('./login');
+const signup = require('./signup');
 const users = require('./users');
 
+router.use('/', login);
+router.use('/', signup);
 router.use('/', users);
 
 module.exports = router;
