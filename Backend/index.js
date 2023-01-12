7968c33c031afa24c64df12d46b3dcd234321ae0
Tamar <tamartda@gmail.com>
2023-01-12 23:03:13 +0200

    more changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes/index');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const limiter = require("./middleware/limiter");
+const routes = require("./routes/index");
 
+dotenv.config();
+
+const { PORT = 3000, NODE_ENV, DB_ADRESS } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/survey', ()=> {
+app.use(helmet());
+app.use(cors());
+app.options("*", cors());
+
+mongoose.connect(
+  NODE_ENV === "production" ? DB_ADRESS : "mongodb+srv://db_admin:FsYxEvQt9NSPaSQ7@bioactivitiescluster.jsexwee.mongodb.net/RMSTest",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
     console.log("connected");
-});
+  }
+);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use(limiter);
 
+app.use("/", routes);
 
-app.get('*', (req, res) => {
-    res.status(404).send({ message: 'Requested resource not found' });
+app.get("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "An error occurred on the server" : message,
   });
-     
-  app.use(errors());
-  
-  app.use((err, req, res, next) => {
-    const { statusCode = 500, message } = err;
-  
-    res.status(statusCode).send({
-      message: statusCode === 500 ? 'An error occurred on the server' : message,
-    });
-  });
+});
 
-const port = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`Listening on port ${port}`));
+//const port = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
