c465f7f46f7b08333bb50f7a6755e84b2e74eeb9
zemira kehati <kehati10@gmail.com>
2023-01-24 03:17:20 +0200

    added recommendation route to index of all routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,12 +4,16 @@ const auth = require('../middleware/auth');
 const login = require('./login');
 const signup = require('./signup');
 const users = require('./users');
-const authResponse = require('./createAuthResponse');
 const allResponses = require('./allResponses');
+const product_scores = require('./product_scores')
+const recommedation = require('./recommedation')
+const authResponse = require('./createAuthResponse');
 
 router.use('/', signup);
 router.use('/', allResponses);
 router.use('/', users);
+router.use('/', product_scores);
+router.use('/', recommedation);
 router.use(auth);
 router.use('/', login);
 router.use('/', authResponse);
